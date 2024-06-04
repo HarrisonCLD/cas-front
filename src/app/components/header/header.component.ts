5eba0a42b70b5e9c920d71e950d3214336768671
CLAUDEL HARRISON <harrison.claudel@univ-reims.fr>
2024-06-04 11:45:47 +0200

    maj

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'header',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public userService = inject(UserService);
 }
